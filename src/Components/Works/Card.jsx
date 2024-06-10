b4f72d15fb442779ed0d3f459ac0d2c0786bb84e
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2024-06-11 00:12:22 +0200

    smoth

--- src/Components/Works/Card.jsx
+++ src/Components/Works/Card.jsx
@@ -20,8 +20,8 @@ const Card = () => {
   };
 
   const handleClick = () => {
-    smoothscroll.polyfill();
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    
+    window.scrollTo({ top: 0, behavior: 'smoth' });
   };
 
   return (
