5f7e0aad294c461195c6cae79e0b1fce863386d8
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2025-12-14 20:55:46 +0100

    gap

--- src/Components/Works/Latest.jsx
+++ src/Components/Works/Latest.jsx
@@ -37,7 +37,7 @@ const Latest = () => {
   };
 
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-4 gap-12 gap-y-16 mt-8">
+    <div className="grid grid-cols-2 lg:grid-cols-4 gap-8 gap-y-16 mt-8">
       {works.slice(0, 4).map((work, index) => (
         <div key={index}>
           <div className="relative overflow-hidden group">
